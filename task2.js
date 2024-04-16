f5a18928e3358db2003861152708e851f8416310
Mike-Watkins-Ada <167078886+Mike-Watkins-Ada@users.noreply.github.com>
2024-04-16 11:25:50 +0000

    task upto 4

--- task2.js
+++ task2.js
@@ -7,3 +7,5 @@ for (let i = 1; i <= 3; i++) {
     console.log(Math.pow(i, 2))
 
 } 
+
+
---
+++ task3.js
@@ -0,0 +1,7 @@
+console.log("Task 3")
+
+// Use a loop to print the numbers from 10 to 1 (in descending order)
+
+for ( i = 10; i > 0; i--) {  // i = i - 1 == i--
+    console.log ( i )
+}
