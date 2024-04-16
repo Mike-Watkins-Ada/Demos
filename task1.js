433df71cf8fc0548ad21b946b04b2997bff7ce3a
Mike-Watkins-Ada <167078886+Mike-Watkins-Ada@users.noreply.github.com>
2024-04-16 09:42:56 +0000

    Initial Setup

---
+++ task1.js
@@ -0,0 +1,8 @@
+const readline = require("readline-sync");
+
+let userValue = readline.question("Provide value: ");
+
+
+console.log("Hi, you entered " + userValue);
+console.log(`Hi, you entered ${userValue}`);
+
