6f013490c22657aa630e8270977a20a5ec71b832
Mike-Watkins-Ada <167078886+Mike-Watkins-Ada@users.noreply.github.com>
2024-04-16 12:15:06 +0000

    Updated with task 5

--- task4.js
+++ task4.js
@@ -1,1 +1,15 @@
 console.log("Task 4")
+
+//Use a loop to print the numbers between 1 and 1000 that are divisible by 3 and 4.
+for (let i = 1; i <= 15; i++) {
+    let calc = i%3;
+    if(calc == 0) {
+        console.log(`${i} is divisable by 3 = ${calc}`)
+    }
+
+    calc = i%4;
+    if(calc == 0) {
+        console.log(`${i} is divisable by 4 = ${calc}`)
+    }
+
+}
