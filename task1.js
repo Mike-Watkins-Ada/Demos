8196a46bde891ee2ad1c0c2e5be4051486cce243
Mike-Watkins-Ada <167078886+Mike-Watkins-Ada@users.noreply.github.com>
2024-04-16 11:19:07 +0000

    Upto task 2

--- task1.js
+++ task1.js
@@ -1,8 +1,23 @@
+
+// Use a loop to print out the 7 times table up to 12 x 7 
+// (extension: let the user choose which times table they 
+// would like to see.)
+
 const readline = require("readline-sync");
+let table = readline.question("Provide value: ");
 
-let userValue = readline.question("Provide value: ");
+//let table = userValue;
+for(let i = 1; i <= 12; i++) {
+    let value = i * table;
+    console.log(`${i} x ${table} = ${value}`)
+}
 
 
-console.log("Hi, you entered " + userValue);
-console.log(`Hi, you entered ${userValue}`);
+// const readline = require("readline-sync");
+
+// let userValue = readline.question("Provide value: ");
+
+
+// console.log("Hi, you entered " + userValue);
+// console.log(`Hi, you entered ${userValue}`);
 
---
+++ task2.js
@@ -0,0 +1,9 @@
+console.log("Task2")
+
+// Use a loop to print out the squares for the numbers between 1 and 100.
+
+for (let i = 1; i <= 3; i++) {
+    // this is where I loop
+    console.log(Math.pow(i, 2))
+
+} 
